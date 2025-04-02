184ce1b2d9ba2f2942cdef1ef0543f9981b84acf
oscar <osx@Oscars-MacBook-Pro-2.local>
2025-04-02 13:49:56 +0700

    debug env

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -3,6 +3,18 @@ import dotenv from "dotenv";
 dotenv.config();
 dotenv.config({ path: `.env.local`, override: true });
 
+console.log({
+  PORT: process.env.PORT,
+  POSTGRES_USER: process.env.POSTGRES_USER,
+  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+  POSTGRES_DB: process.env.POSTGRES_DB,
+  POSTGRES_HOST: process.env.POSTGRES_HOST,
+  POSTGRES_URL: process.env.POSTGRES_URL,
+  POSTGRES_PORT: parseInt(process.env.POSTGRES_PORT || "5432"),
+  JWT_ACCESS_TOKEN_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET || "",
+  JWT_REFRESH_TOKEN_SECRET: process.env.JWT_REFRESH_TOKEN_SECRET || "",
+})
+
 export const config = {
   PORT: process.env.PORT,
   POSTGRES_USER: process.env.POSTGRES_USER,
